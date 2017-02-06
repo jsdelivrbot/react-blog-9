0b591b606f120ba74a7fad97f1ad7882f29d1de0
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-06 14:58:26 +0100

    Set up Router history and root route. Extracting routes into separate file.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
+  // Passing off control of rendering our application to Router...
+  // Router is the object that decides which React components to render whenever the URL changes
+  // configuration: browserHistory is an object that tells React Router how to interpret changes to the URL
+  // browserHistory is 1 out of 3 histories we can use
+  // browserHistory --> http://www.blog.com/posts/5 --> if anything changes after www.blog.com/ the History tells React Router that it needs to update
+  // hashHistory    --> http://www.blog.com/#posts/5 --> if anything changes after hash the History tells React Router that it needs to update
+  // memoryHistory  --> doesn't uses the URL at all for triggering React Router...
+  // more config --> telling Router what valid routes our app has --> see routes.js
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
