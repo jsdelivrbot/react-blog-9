4680668700145646360bcc487e2b8c5ab1ad57c7
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-07 16:19:56 +0100

    Implement dynamic show action route (/posts/:id).

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts-index';
 import PostsNew from './components/posts-new';
+import PostsShow from './components/posts-show';
 
 // We can use <Route /> as a self closing tag but we don't have to.
 export default (
   // We have specified that Greeting component (child) has to render within App...
   // but React doesn't know where within App to render it...
   // we need to specify {this.props.children} within app.js
+  // posts/:id · react-router will parse the URL and pass a this.props.params.id into PostsShow component
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
