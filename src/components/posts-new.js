bb428b5d8577cfc66b6514b322476af21679938f
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-07 11:51:38 +0100

    · Configuring redux-form for PostsNewForm. · Creating the actual form.

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form'; // reduxForm object is almost identical to the connect() function from react-redux
+
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Create Form</div>
+      <form>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// Passing in reduxForm configuration...
+// · what the form is going to be called
+// · the name of the fields reduxForm needs to be in charge
+export default reduxForm({
+  form: 'PostsNewForm', // The name of the form doesn't need to match the name of the component
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
+// Whenever the user changes the values of any of these inputs, redux form sets those values in our global application state
+// state === {
+//   form: {
+//     PostsNewForm: {
+//       title: '....',
+//       categories: '....',
+//       content: '....'
+//     }
+//   }
+// }
+
+// In our older apps we used to record form user input state locally in the component
+// redux-form records the form 'state' into global state instead
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './posts-reducer';
+import { reducer as formReducer } from 'redux-form'; // import redux-form · grab it's reducer property · create variable formReducer with it
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
