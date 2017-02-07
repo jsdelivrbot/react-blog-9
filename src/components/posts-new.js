54fcbdd312667df6a5c811270c1d9e9dabd40fa9
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-07 14:00:50 +0100

    Can create new posts via the API.
    No form validations implemented yet.

--- src/components/posts-new.js
+++ src/components/posts-new.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form'; // reduxForm object is almost identical to the connect() function from react-redux
-
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
@@ -11,9 +11,11 @@ class PostsNew extends Component {
 
     return (
       // onSubmit={handleSubmit} · on form submission the control is passed onto redux-form (to run it's validations etc.)
+      // Optionally we can pass an action creator to handleSubmit(someActionCreator) which will be called (with the properties from the form) when the form gets submitted (and is valid)
       // {...title} · binding a specific form field to the relevant redx-form configuration object
       // {...title} · 'de-structuring' the title object · every single property (key + value) of title object will be visible on the <input>
-      <form onSubmit={handleSubmit}>
+      // Note: can also pass in an optional
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -36,6 +38,10 @@ class PostsNew extends Component {
   }
 }
 
+
+// connect(): 1st argument is mapStateToProps · 2nd argument is mapDispatchToProps
+// reduxForm(): 1st argument is the config · 2nd argument is mapStateToProps · 3rd argument is mapDispatchToProps
+
 // Passing in reduxForm configuration...
 // · what the form is going to be called
 // · the name of the fields reduxForm needs to be in charge
@@ -44,7 +50,7 @@ class PostsNew extends Component {
 export default reduxForm({
   form: 'PostsNewForm', // The name of the form doesn't need to match the name of the component
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew); // { createPost } === { createPost: createPost } === shorthand for mapDispatchToProps()...
 // Whenever the user changes the values of any of these inputs, redux form sets those values in our global application state
 // state === {
 //   form: {
